fd486534ac2956e52c7526d5e11edd71e07d9ecd
Vitaliy <Vitaliy@yandex.ru>
2024-06-23 15:36:34 +0300

    lesson_49: Представление категорий

--- python_blog/views.py
+++ python_blog/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request) -> HttpResponse:
-    return HttpResponse("Привет, Python315!") # вернет страницу с этой надписью
+    """
+    Представление для главной страницы.
+    """
+    return HttpResponse(
+        """<h1>Мой блог!</h1>
+        <a href="/category/">Категории</a>
+        """
+    )  # вернет страницу с заголовком с этой надписью
+
+
+def category(request) -> HttpResponse:
+    """
+    Представление для категорий
+    """
+    return HttpResponse(
+        """<ul><li>Python</li><li>Django</li><li>Flask</li></ul>
+        <a href="/">На главную</a>
+        """
+    )  # Вернет страницу со списком
