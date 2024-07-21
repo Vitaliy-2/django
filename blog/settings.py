810b806278e315bb9dd626e30a8d96af5b1a5374
Vitaliy <Vitaliy@yandex.ru>
2024-07-21 12:52:29 +0300

    models: установка shell plus

--- blog/settings.py
+++ blog/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # другие приложения
+    'django_extensions',
     # Пользовательские приложения внизу!
     'python_blog', 
 ]
